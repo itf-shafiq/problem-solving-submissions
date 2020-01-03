3b398d65eb591765b72614edad71eae1cb8d75dc
Shafiq <itf.shafiq@gmail.com>
2020-01-03 21:36:53 +0600

    1st problem solved successfully

--- if-else-problem-solving.py
+++ if-else-problem-solving.py
@@ -2,20 +2,32 @@
 # Source: HackerRank.com
 
 '''Task
-Given an integer, , perform the following conditional actions:
+Given an integer,N , perform the following conditional actions:
 
-If  is odd, print Weird
-If  is even and in the inclusive range of  to , print Not Weird
-If  is even and in the inclusive range of  to , print Weird
-If  is even and greater than , print Not Weird
+If N is odd, print Weird
+If N is even and in the inclusive range of 2 to 5, print Not Weird
+If N  is even and in the inclusive range of 6 to 20, print Weird
+If N is even and greater than 20, print Not Weird
 Input Format
 
-A single line containing a positive integer, .
+A single line containing a positive integer, n.
 
 Constraints
+1<= 0 <= 100
 
 Output Format
 
 Print Weird if the number is weird; otherwise, print Not Weird.'''
 
 # Start Solving this problem
+
+n = int(input().strip())
+
+if n%2 == 0 and n in range(2,6):
+    print("Not Weird")
+elif n%2 == 0 and n in range(6,21):
+    print("Weird")
+elif n%2 == 0 and n > 20:
+    print("Not Weird")
+else:
+    print("Weird")
